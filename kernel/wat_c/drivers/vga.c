a49cfcd04f916e0d5e27b2e90e959fcae2e13123
Rouvik Maji <majirouvik@gmail.com>
2024-12-29 20:55:01 +0530

    Fixed a bug, VGA_setVideoMode didnt return success status

--- kernel/wat_c/drivers/vga.c
+++ kernel/wat_c/drivers/vga.c
@@ -2,7 +2,8 @@
 
 uint16_t VGA_setVideoMode(uint16_t mode)
 {
-    x86_setVideoMode(mode);                                                 // call the underlying x86 10h interrupt in real mode
+    uint16_t ret = x86_setVideoMode(mode);                                                 // call the underlying x86 10h interrupt in real mode
     IDT_LoadIDTTable();                                                     // load the interrupt table to idtr
     IRQ_Initialise();                                                       // initialise the Interrupt requests
+    return ret;
 }
